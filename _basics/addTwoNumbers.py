8eb2d5ba5ce1bd561dbe33250fb87135947b34bb
ShibaYui <ai.brs.miku@gmail.com>
2025-01-12 15:10:08 +0800

    2025-01-12

---
+++ _basics/addTwoNumbers.py
@@ -0,0 +1,13 @@
+##	Filename: addTwoNumbers.py
+##	Author: yu1
+##	Date: 2025-01-12 14:53:40 +0800
+## 
+##	Description:
+## 
+
+num_1=input('Enter 1st number:')
+num_2=input('Enter 2nd number:')
+
+sum=float(num_1)+float(num_2)
+
+print('{0}+{1}, Ans: {2}'.format(num_1, num_2, sum))
