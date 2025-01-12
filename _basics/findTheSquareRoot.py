ccd11348026763f36d3fa1fc3f9b921c04019ce4
ShibaYui <ai.brs.miku@gmail.com>
2025-01-12 15:52:19 +0800

    2025-01-12

---
+++ _basics/findTheSquareRoot.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##	Filename: findTheSquareRoot.py
+##	Author: yu1
+##	Date: 2025-01-12 15:31:39 +0800
+## 
+##	Description:
+## 
+
+num=input('Enter number:')
+
+square=float(num)**0.5
+
+print(f'{num}square is {square}')
+
